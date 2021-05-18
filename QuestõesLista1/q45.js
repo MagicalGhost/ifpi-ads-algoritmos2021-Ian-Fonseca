bdcf0ef4c4d1bde791ccade10e38e9d9675bbbc7
Ian <ianjose094@gmail.com>
2021-05-18 13:23:06 -0300

    grr

--- QuestõesLista1/q45.js
+++ QuestõesLista1/q45.js
@@ -4,4 +4,4 @@ var not50 = (saque - saque%50)/50;
 var not10 = ((saque - not50 * 50) - (saque - not50 * 50)%10)/10;
 var not5 = (saque - (((saque - (not50 * 50 + not10 * 10))%5) + (not10 * 10) + (not50 * 50)))/5;
 var not1 = saque - (((saque - (((saque - (not50 * 50 + not10 * 10))%5) + (not10 * 10) + (not50 * 50)))) + (((saque - not50 * 50) - (saque - not50 * 50)%10)) + ((saque - saque%50)))
-console.log(((saque - saque%50)/50), 'nota(s) de R$ 50,00\n', (not10), 'nota(s) de R$ 10,00\n', (not5), 'nota(s) de R$ 5,00\n', (not1), 'nota(s) de R$ 1,00');
+console.log((not50), 'nota(s) de R$ 50,00\n', (not10), 'nota(s) de R$ 10,00\n', (not5), 'nota(s) de R$ 5,00\n', (not1), 'nota(s) de R$ 1,00');
