7b4e39e1e0522e93eb037f6df0e565a3a9e62045
Ian <ianjose094@gmail.com>
2021-05-18 13:21:22 -0300

    lista

--- lista.js
+++ lista.js
@@ -193,7 +193,17 @@ console.log('A quantidade informada é equivalente a: ', ((dias - dias%365)/365)
 /*var num1 = Number(input('Informe o primeiro numerador: '));
 var den1 = Number(input('Informe o primeiro denominador: '));
 var num2 = Number(input('Informe o segundo numerador: '));
-var den2 = Number(input('Informe o segundo denominador: '));*/
+var den2 = Number(input('Informe o segundo denominador: '));
+var resto;
+var x = den1;
+var y = den2;
+while(resto != 0){
+    resto = x%y;
+    x = y;
+    y = resto;
+}
+var soma = ((((den1 * den2)/x)/den1) * num1) + ((((den1 * den2)/x)/den2) * num2);
+console.log('A soma de ambas as frações fornecidas em forma de fração é:\n', (soma), '\n---\n', ((den1 * den2)/x));*/
 
 // ### QUESTÃO 39 ###
 /*var a = Number(input('Informe um valor para A: '));
