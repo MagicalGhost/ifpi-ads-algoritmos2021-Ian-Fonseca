51651899d12f0f48aeef525ba23645634a46d155
Ian <ianjose094@gmail.com>
2021-06-08 00:10:11 -0300

    primeira questão concluida

--- ProvaPratica/q01_mediaCOVID.js
+++ ProvaPratica/q01_mediaCOVID.js
@@ -1,19 +1,18 @@
 const input = require('prompt-sync')();
 
 function main() {
-    var media_de_casos_14_dias = Number(input('Informe a média de casos há 14 dias: '));
-    var media_de_casos_hoje = Number(input('Informe a média de casos hoje: '));
-    var variacao = media_de_casos_14_dias - media_de_casos_hoje;
-    var porcentagem = ((100 * variacao)/(media_de_casos_14_dias + media_de_casos_hoje));
+    var media_de_casos_14_dias = Number(input('Informe a média de casos há 14 dias: ')); //recebe o valor dos casos nos ultimos 14 dias
+    var media_de_casos_hoje = Number(input('Informe a média de casos hoje: ')); //recebe o valor nos casos de hoje
+    var variacao = media_de_casos_14_dias - media_de_casos_hoje; // faz a diferença dos casos dos ultimos 14 dias com os casos de hoje
+    var variacao_em_porcentagem = (variacao * 100)/(media_de_casos_14_dias + media_de_casos_hoje); // faz a porcentagem da variação pelo valor total de casos(casos ha 14 dias + casos de hoje), se a variação for negativa = porcentagem negativa, se positiva = porcentagem positiva
 
-    calcular_variacao(porcentagem);
-    console.log('Variação de: ', porcentagem, '%')
+    calcular_variacao(variacao_em_porcentagem); 
 }
 
-function calcular_variacao(pctg) {
-    if (pctg < 15 && pctg > 0) {
+function calcular_variacao(v) { // função para calcular se a porcentagem da variação esta em alta, queda ou estavel
+    if (v < 15 && v > 0) {
         console.log('Em estabilidade');
-    } else if(pctg > 15) {
+    } else if(v > 15) {
         console.log('Em alta');
     } else {
         console.log('Em queda');
