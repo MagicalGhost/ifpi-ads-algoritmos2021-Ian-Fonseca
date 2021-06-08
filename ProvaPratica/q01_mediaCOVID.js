c06515409dd32001f8996dd69b6eabcfdc400aaa
Ian <ianjose094@gmail.com>
2021-06-08 16:16:15 -0300

    tudo concluido

--- ProvaPratica/q01_mediaCOVID.js
+++ ProvaPratica/q01_mediaCOVID.js
@@ -3,19 +3,26 @@ const input = require('prompt-sync')();
 function main() {
     var media_de_casos_14_dias = Number(input('Informe a média de casos há 14 dias: ')); //recebe o valor dos casos nos ultimos 14 dias
     var media_de_casos_hoje = Number(input('Informe a média de casos hoje: ')); //recebe o valor nos casos de hoje
-    var variacao = media_de_casos_14_dias - media_de_casos_hoje; // faz a diferença dos casos dos ultimos 14 dias com os casos de hoje
-    var variacao_em_porcentagem = (variacao * 100)/(media_de_casos_14_dias + media_de_casos_hoje); // faz a porcentagem da variação pelo valor total de casos(casos ha 14 dias + casos de hoje), se a variação for negativa = porcentagem negativa, se positiva = porcentagem positiva
-
-    calcular_variacao(variacao_em_porcentagem); 
+    var variacao = (media_de_casos_hoje * 100)/media_de_casos_14_dias; // faz a porcentagem da variação pelo valor total de casos(casos ha 14 dias + casos de hoje), se a variação for negativa = porcentagem negativa, se positiva = porcentagem positiva
+    
+    calcular_variacao(media_de_casos_hoje, media_de_casos_14_dias, variacao);
 }
 
-function calcular_variacao(v) { // função para calcular se a porcentagem da variação esta em alta, queda ou estavel
-    if (v < 15 && v > 0) {
-        console.log('Em estabilidade');
-    } else if(v > 15) {
-        console.log('Em alta');
+function calcular_variacao(mh, m14, v) { // função para calcular se a porcentagem da variação esta em alta, queda ou estavel, recebe a media de hoje, dos 14 dias e a variação em porcentagem
+    if (mh < m14) {
+        v *= -1;
+        if (v < -15) {
+            console.log(`Em queda\nVariação em porcentagem: ${v}%`);
+        } else {
+            console.log(`Em estabilidade\nVariação em porcentagem: ${v}%`);
+        }
     } else {
-        console.log('Em queda');
+        v -= 100;
+        if (v > 15) {
+            console.log(`Em alta\nVariação em porcentagem: ${v}%`);
+        } else {
+            console.log(`Em estabilidade\nVariação em porcentagem: ${v}%`);
+        }
     }
 }
 
