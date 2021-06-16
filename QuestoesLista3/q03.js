52e87289ac585a2ab44c0cd355e20d1974ec63c9
Ian <ianjose094@gmail.com>
2021-06-16 00:27:57 -0300

    q3 completed

--- QuestoesLista3/q03.js
+++ QuestoesLista3/q03.js
@@ -1,7 +1,25 @@
-const input = require('prompt-sync')();  // INCOMPLETA
+const input = require('prompt-sync')();
 
 function main() {
-    
+    var num1 = Number(input('Informe o primeiro número: '));
+    var num2 = Number(input('Informe o segundo número: '));
+
+    mdc(num1, num2);
+}
+
+function mdc(n1, n2) {
+    var i = 2;
+    var xmdc = 1;
+    while(n1/i != 1 && n2/i != 1) {
+        if (n1%i == 0 && n2%i == 0) {
+            xmdc *= i;
+            n1 = n1/i;
+            n2 = n2/i;
+        } else {
+            i++;
+        }
+    }
+    console.log(xmdc);
 }
 
 main();
