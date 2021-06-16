56ade0491726fa340b90f6ca7780be9b7bfd6d8f
Ian <ianjose094@gmail.com>
2021-06-16 10:33:43 -0300

    dinossauro

--- QuestõesLista4/q01.js
+++ QuestõesLista4/q01.js
@@ -1,16 +1,18 @@
 const input = require('prompt-sync')();
 
 function main() {
-    var num = Number(input('Informe um número: '));
+    let num = Number(input('Informe um número: '));
+    let i = 0;
 
     while(num != 0) {
         console.log(`Número lido: ${num}\nLista de divisores:`);
 
         if(num >= 0) {
-            for (let i = 0; i <= num; i++) {
+            while(i <= num) {
                 if(num%i == 0) {
                     console.log(i);
                 }
+                i++;
             }
         } else {
             console.log('Digite apenas valores positivos!');
